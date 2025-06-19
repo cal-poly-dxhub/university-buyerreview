aedd6f2b7df2cb2a2a061af7562abb9a89be39a5
Belal Elshenety <belal.elshenety@gmail.com>
2025-06-19 11:15:03 -0700

    Removed old folders since they were already refactored

--- LLM_Extraction/general_doc_parser.py
+++
@@ -1,9 +0,0 @@
-from prompts import general_doc_prompt
-from utils import try_parse_json_like, query_bedrock_with_multiple_pdfs
-from model_registry import ModelRegistry
-
-def parse_documents_together(files):
-    result_text = query_bedrock_with_multiple_pdfs(
-        general_doc_prompt, files, model_id=ModelRegistry.sonnet_3_7)
-    parsed = try_parse_json_like(result_text)
-    return parsed if parsed else {"error": "Failed to parse JSON", "raw": result_text}

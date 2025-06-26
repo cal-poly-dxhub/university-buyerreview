a60bf329aa04cb994f022636955279331e30802f
Adarsh132500 <adarsmuru@gmail.com>
2025-06-26 00:37:13 -0700

    feat: data security with embedding

--- Paralled_MAS/Graphs/data_sec_only.py
+++ Paralled_MAS/Graphs/data_sec_only.py
@@ -1,20 +1,16 @@
 from langgraph.graph import StateGraph, END
 from langchain_core.runnables import RunnableLambda
-from Agents.doc_parser import parse_documents_parallel
+from Agents.doc_parser import parse_documents_node
 from Agents.data_sec_classification import run_data_sec_classification
 from state import PipelineState
-
-async def parse_documents_node(state: PipelineState) -> PipelineState:
-    parsed = await parse_documents_parallel(state["uploaded_files"])
-    return {"parsed_data": parsed}
 
 def build_data_sec_graph():
     graph = StateGraph(PipelineState)
     graph.add_node("Parse Documents", RunnableLambda(parse_documents_node))
-    graph.add_node("Run Data Decurity Classification", RunnableLambda(run_data_sec_classification))
+    graph.add_node("Run Data Security Classification", RunnableLambda(run_data_sec_classification))
 
     graph.set_entry_point("Parse Documents")
-    graph.add_edge("Parse Documents", "Run Data Decurity Classification")
-    graph.add_edge("Run Data Decurity Classification", END)
+    graph.add_edge("Parse Documents", "Run Data Security Classification")
+    graph.add_edge("Run Data Security Classification", END)
 
     return graph.compile()
